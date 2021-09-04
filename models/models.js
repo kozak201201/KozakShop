69eadbee45f8f89ea4ee8d3a2a1929bccba6174b
kozak201201 <kozak20012001@gmail.com>
2021-09-04 16:57:06 +0300

    create models in models.js

---
+++ models/models.js
@@ -0,0 +1,44 @@
+const db = require('../db');
+const {DataTypes} = require('sequelize');
+
+const User = db.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    login: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING, allowNull: false}
+});
+
+const Product = db.define('product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.TEXT, allowNull: false}
+});
+
+const Category = db.define('category', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+});
+
+const Manufacture = db.define('manufacture', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+});
+
+const UserProduct = db.define('user_product', {
+    count: {type: DataTypes.INTEGER}
+});
+
+Category.hasMany(Product);
+Manufacture.hasMany(Product);
+
+User.belongsToMany(Product, {through: UserProduct});
+Product.belongsToMany(User, {through: UserProduct});
+
+module.exports = {
+    User,
+    Product,
+    Category,
+    Manufacture,
+    UserProduct
+}
