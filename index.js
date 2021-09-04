7d7fc5f7f85a73142c65aace55e3b35c359627bf
kozak201201 <kozak20012001@gmail.com>
2021-09-04 16:44:37 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 require('dotenv').config();
 
+const db = require('./db');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`server has been started on ${PORT}`);
-});
+const start = async() => {
+    await db.authenticate();
+    await db.sync();
+
+    app.listen(PORT, () => {
+        console.log(`server has been started on ${PORT}`);
+    });
+}
+
+start();
+
 
 app.use(express.json());
